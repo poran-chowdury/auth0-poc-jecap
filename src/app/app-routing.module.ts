d4bfd66c7545bd5c8898fe86ccb18d22bd1195f0
poran-chowdury <poran.chowdury@selise.ch>
2023-06-11 17:06:25 +0600

    reformat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from "./app.component";
 import {LoginComponent} from "./login/login.component";
 import {SignupComponent} from "./signup/signup.component";
@@ -7,15 +7,16 @@ import {DashboardComponent} from "./dashboard/dashboard.component";
 import {QrcodeComponent} from "./qrcode/qrcode.component";
 
 const routes: Routes = [
-  {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent},
-  {path:'dashboard',component:DashboardComponent},
-  {path:'logout',component:LoginComponent},
-  {path:'qr',component:QrcodeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'logout', component: LoginComponent},
+  {path: 'qr', component: QrcodeComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
